4531792961265dcd0e6f80afc758cc48d7041a61
CN112 <cn112@CN112.local>
2019-06-03 14:40:06 +0100

    Added new navbar + Smooth scroll

--- src/sitemain.js
+++ src/sitemain.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from './components/navbar';
+//import Navbar from './components/navbar';
 import TopBanner from './components/topbanner';
 import AboutMe from './components/aboutme'
 import Education from './components/education'
 import Experiance from './components/experiance'
 import Projects from './components/projects'
 import Interests from './components/interest'
+import Navbar from './components/NewNavbar'
+
 
 class MainSite extends React.Component{
     render(){
@@ -13,6 +15,7 @@ class MainSite extends React.Component{
             <div>
                 <TopBanner/>
                 <Navbar/>
+                
                 <AboutMe/>
                 <Education/>
                 <Experiance/>
